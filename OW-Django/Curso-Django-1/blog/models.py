3057245495d69c8b3a78a3563c39bff7a290735a
LeonardoXTP <tnd.leonardo@gmail.com>
2024-04-26 16:19:07 +0200

    Integración de MySQL y creación de la primera clase (tabla)

--- OW-Django/Curso-Django-1/blog/models.py
+++ OW-Django/Curso-Django-1/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Post(models.Model):
+
+    titulo = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250) #id que permite encontrar un titulo largo con guiones en lugar de espacios
+    body = models.TextField()
+    
+
 # Create your models here.
