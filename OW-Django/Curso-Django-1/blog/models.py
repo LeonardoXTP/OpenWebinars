42476ff7b299f89af070c39688a8987a4162210b
LeonardoXTP <tnd.leonardo@gmail.com>
2024-04-26 16:38:47 +0200

    Creacion de un valor en la shell y agregación de nuevo campo en models

--- OW-Django/Curso-Django-1/blog/models.py
+++ OW-Django/Curso-Django-1/blog/models.py
@@ -5,6 +5,6 @@ class Post(models.Model):
     titulo = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250) #id que permite encontrar un titulo largo con guiones en lugar de espacios
     body = models.TextField()
-    
+    publisher = models.BooleanField(default=False)
 
 # Create your models here.
