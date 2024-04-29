319ad550a29528666c92f9ea16a3343b3247e3ab
LeonardoXTP <tnd.leonardo@gmail.com>
2024-04-29 11:56:25 +0200

    Creacion de html lista y detalles y otros cambios

--- OW-Django/Curso-Django-1/blog/views.py
+++ OW-Django/Curso-Django-1/blog/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, HttpResponse
+from django.http import Http404
 from .models import Post
 
 
 def post_list(request):
     posts = Post.objects.all()
-    template = 'blog/post/list.html'
+    template = 'post/lista.html'
     context = {
         'posts': posts,
     }
 
     return render(request, template, context)
+
+
+def post_detail(request, id):
+    template = 'post/detalles.html'
+    try:
+        post = Post.objects.get(id=id)
+        context = {
+            'post': post,
+        }
+    except Post.DoesNotExist:
+        raise Http404("Publicación no encontrada")
+    
+    return render(request, template, context)
+
