dda5dbbebc088e9f978852478f2177a8c91fc61f
LeonardoXTP <tnd.leonardo@gmail.com>
2024-05-13 10:34:21 +0200

    login y logout

--- OW-Django/Curso-Django-1/myproject/settings.py
+++ OW-Django/Curso-Django-1/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Custom redirect
+
+LOGIN_REDIRECT_URL = 'blog:create_post'
+LOGIN_URL = 'login'
